f1ffb86fcfff8372e455cb31efd2cdb51fcfc766
Tibium <143140444+Tibium@users.noreply.github.com>
2024-02-12 20:46:47 -0500

    Update pong_game.py

--- pong_game.py
+++ pong_game.py
@@ -1,37 +1,107 @@
-import remi.gui as gui
-from remi import start, App
-import random
-import player_controls
+import PySimpleGUIWeb as sg
+import datetime
+import keyboard
+from pong_classes import BACKGROUND_COLOR, Bat, Ball, Scores, GAMEPLAY_SIZE, BAT_SIZE
+from pong_functions import check_ball_exit, goto_menu, update_leaderboard
 
-class PongApp(App):
-    def __init__(self, *args):
-        super(PongApp, self).__init__(*args)
-      
-    def main(self):
-        # Creating a container for the user to view/see
-        container = gui.VBox(width=600, height=400, style={'position':'relative', 'overflow':'hidden'})
+def pong():
+    sleep_time = 10
 
-        # Creating paddles and ball
-        self.paddle1 = gui.Label(width=10, height=60, style={'background-color': 'blue', 'position': 'absolute', 'left': '10px', 'top': '170px'})
-        self.paddle2 = gui.Label(width=10, height=60, style={'background-color': 'red', 'position': 'absolute', 'right': '10px', 'top': '170px'})
-        self.ball = gui.Label(width=10, height=10, style={'background-color': 'green', 'position': 'absolute', 'top': '195px', 'left': '295px'})
+    leaderboard_layout = [
+        [sg.Text("Leaderboard", font="Courier 20", justification="center")],
+        [sg.Listbox(values=[], size=(20, 20), key="-LEADERBOARD-", enable_events=True)]
+    ]
+    
+    inner_layout = [[sg.Graph(GAMEPLAY_SIZE,
+                        (0, GAMEPLAY_SIZE[1]),
+                        (GAMEPLAY_SIZE[0], 0),
+                        background_color=BACKGROUND_COLOR,
+                        key='-GRAPH-')],
+              [sg.Button('Back to Menu', key="-MENU-")]]
 
-        # Adding paddle and ball to the main container
-        container.append(self.paddle1)
-        container.append(self.paddle2)
-        container.append(self.ball)
+    main_menu_layout = [[sg.Text("Pong", font="Courier 40", justification="center", size=(10, 1))],
+                        [sg.Text("", font="Courier 8")],
+                        [sg.Text("-- Instructions --", font="Courier 16")],
+                        [sg.Text("Left player controls: W and S", font="Courier 12")],
+                        [sg.Text("Right player controls: I and K", font="Courier 12")],
+                        [sg.Text("Quit to leave the game", font="Courier 12")],
+                        [sg.Text("", font="Courier 8")],
+                        [sg.Text("Winner is first to 10 points", font="Courier 12")],
+                        [sg.Text("", font="Courier 8")],
+                        [sg.Button("Start", key='-START-', font="Courier 24"),
+                        sg.Button("Quit", key='-QUIT-', font="Courier 24")]]
+    
+    layout = [[sg.Column(main_menu_layout, key='-MAIN_MENU-', size=GAMEPLAY_SIZE),
+           sg.Column(inner_layout, key='-GAME-'), sg.Column(leaderboard_layout, key='-LEADERBORD-')]]
+    
+    window = sg.Window('Pong', layout, finalize=True, use_default_focus=False, web_port=8080, web_debug=True, web_start_browser=True, return_key_down_events=True)
+    
+    graph_elem = window['-GRAPH-']  
+    
+    scores = Scores(graph_elem)
+    bat_1 = Bat(graph_elem, 'green', 30, GAMEPLAY_SIZE[1])
+    bat_2 = Bat(graph_elem, 'yellow', GAMEPLAY_SIZE[0] - 30 - BAT_SIZE[0], GAMEPLAY_SIZE[1])
+    ball_1 = Ball(graph_elem, bat_1, bat_2, 'green1')
 
-        # TODO: Capture Keypresses 
+    start = datetime.datetime.now()
+    last_post_read_time = start
 
-        # Setup player controls
-        self.player_controls = PlayerControls(self.paddle1, self.paddle2, 600)
+    game_playing = False
+    
+    while True:
+        pre_read_time = datetime.datetime.now()
+        processing_time = (pre_read_time - last_post_read_time).total_seconds()
+        time_to_sleep = sleep_time - int(processing_time*1000)
+        time_to_sleep = max(time_to_sleep, 0)
 
-        # TODO: Add game logic for ball movement and collision detection
+        event, values = window.read(time_to_sleep)
+        now = datetime.datetime.now()
+        delta = (now-last_post_read_time).total_seconds()
+        last_post_read_time = now
+        if event in (sg.WIN_CLOSED, "-QUIT-"):
+            break
+        elif event == "-START-":
+            scores.reset()
+            ball_1.restart()
+            window['-MAIN_MENU-'].Visible=False
+            window['-GAME-'].Visible=True
+            last_post_read_time = datetime.datetime.now()
+            game_playing = True
+        elif event == "-MENU-":
+            game_playing = False
+            goto_menu(window)
+        elif game_playing:
+            
+            if keyboard.is_pressed("W"):
+                bat_1.up()
+                
+            elif keyboard.is_pressed("S"):    
+                bat_1.down()
+                    
+            elif keyboard.is_pressed("I"):    
+                bat_2.up()
+                
+            elif keyboard.is_pressed("K"):    
+                bat_2.down()
+                
+        if game_playing:
+            ball_1.update(delta)
+            bat_1.update(delta)
+            bat_2.update(delta)
 
-        return container
+            check_ball_exit(ball_1, scores)
 
-    def on_key_down(self, widget, value, *args):
-      # TODO: read key presses and allow player to move
+            winner = scores.win_loss_check()
+            if winner is not None:
+                winner_name = sg.popup_get_text('Game Over', f'{winner} won! Enter your name: ', no_titlebar=True)
+                if winner_name:
+                    leaderboard = update_leaderboard(winner_name)
+                    window['-LEADERBOARD-'].update(values=leaderboard)
+                game_playing = False
+                goto_menu(window)
 
-      
-start(PongGame, address='0.0.0.0', port=8081, start_browser=True)
+    window.close()
+
+
+if __name__ == '__main__':
+    pong()
